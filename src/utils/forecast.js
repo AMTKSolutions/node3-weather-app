8d9ec34f5277c0d11851d46abf19d4b596ff4421
Anita Trpenoska <anita.trpenoska@gmail.com>
2019-09-17 16:58:42 +0200

    Add Min and Max temperatures to forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -34,7 +34,8 @@ const forecast = (latitude,longitude,callback) => {
             callback ('Unable to find location',undefined)
         }
         else{
-            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. ' + ' The max temperature is ' + body.daily.data[0].temperatureHigh + ' .The min temperature is ' + body.daily.data[0].temperatureLow )
         }           
 
       })
